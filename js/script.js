935a3554c4f74c80c957a286e176abfdacd44f32
Marco.seb <marco.seba96@gmail.com>
2021-02-09 17:41:13 +0100

    Finish first step

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ var app = new Vue({
     el:'#app',
     data:{
         copertine:[]
+    },
+    mounted(){
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then(result =>{
+            this.copertine = result.data.response;
+        });
     }
-
 });
